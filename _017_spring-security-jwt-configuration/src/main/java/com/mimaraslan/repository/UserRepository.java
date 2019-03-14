e205b0a2ebd4d00c12831a624019a4458db5c361
mimaraslan <mimaraslan@gmail.com>
2019-03-14 16:54:16 +0300

    Spring Security

--- _017_spring-security-jwt-configuration/src/main/java/com/mimaraslan/repository/UserRepository.java
+++ _017_spring-security-jwt-configuration/src/main/java/com/mimaraslan/repository/UserRepository.java
@@ -1,4 +1,4 @@
-package com.mimaraslan.service;
+package com.mimaraslan.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
--- _017_spring-security-jwt-configuration/src/main/java/com/mimaraslan/service/DbInit.java
+++ _017_spring-security-jwt-configuration/src/main/java/com/mimaraslan/service/DbInit.java
@@ -5,6 +5,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.mimaraslan.model.User;
+import com.mimaraslan.repository.UserRepository;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,14 +26,14 @@ public class DbInit implements CommandLineRunner {
         // Delete all
         this.userRepository.deleteAll();
 
-        // Crete users
+        // Create users
         User admin = new User("admin",passwordEncoder.encode("admin"),"ADMIN","ACCESS_TEST1,ACCESS_TEST2");
         User katerina = new User("katerina",passwordEncoder.encode("katerina"),"USER","");
         User manager = new User("manager",passwordEncoder.encode("manager"),"MANAGER","ACCESS_TEST1");
 
         List<User> users = Arrays.asList(admin,katerina,manager);
 
-        // Save to db
+        // Save to DB
         this.userRepository.saveAll(users);
     }
 }
