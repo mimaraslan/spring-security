389799cc09268b76cade5e9df94cdb9b2c257121
mimaraslan <mimaraslan@gmail.com>
2019-03-16 12:25:27 +0300

    Spring Security

--- _015_spring-security-jwt-authentication/src/main/java/com/mimaraslan/model/LoginViewModel.java
+++ _015_spring-security-jwt-authentication/src/main/java/com/mimaraslan/model/LoginViewModel.java
@@ -1,23 +1,23 @@
 package com.mimaraslan.model;
 
 public class LoginViewModel {
-	
-    private String username;
-    private String password;
 
-    public String getPassword() {
-        return password;
-    }
+	private String username;
+	private String password;
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public String getUsername() {
-        return username;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
 }
