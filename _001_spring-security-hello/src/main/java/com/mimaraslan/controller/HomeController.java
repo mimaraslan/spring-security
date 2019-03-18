c9098e3312e79e765fb0571f5bd6c63b248b921d
mimaraslan <mimaraslan@gmail.com>
2019-03-18 11:56:30 +0300

    Spring Security

--- _001_spring-security-hello/src/main/java/com/mimaraslan/controller/HomeController.java
+++ _001_spring-security-hello/src/main/java/com/mimaraslan/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class HomeController {
-	
+
 	@GetMapping("index")
 	public String index() {
 		return "index";
--- _001_spring-security-hello/src/main/java/com/mimaraslan/controller/PublicRestApiController.java
+++ _001_spring-security-hello/src/main/java/com/mimaraslan/controller/PublicRestApiController.java
@@ -7,14 +7,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/public")
 public class PublicRestApiController {
- 
-    @GetMapping("test1")
-    public String test1(){
-        return "API Test 1";
-    }
 
-    @GetMapping("test2")
-    public String test2(){
-        return "API Test 2";
-    }
+	@GetMapping("test1")
+	public String test1() {
+		return "API Test 1";
+	}
+
+	@GetMapping("test2")
+	public String test2() {
+		return "API Test 2";
+	}
 }
