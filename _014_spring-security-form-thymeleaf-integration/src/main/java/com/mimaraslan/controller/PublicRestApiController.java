04b72d1f6967163ad681adf6b75886a85041b157
mimaraslan <mimaraslan@gmail.com>
2019-03-22 12:23:55 +0300

    Spring Security

--- _014_spring-security-form-thymeleaf-integration/src/main/java/com/mimaraslan/controller/PublicRestApiController.java
+++ _014_spring-security-form-thymeleaf-integration/src/main/java/com/mimaraslan/controller/PublicRestApiController.java
@@ -12,24 +12,24 @@ import java.util.List;
 @RestController
 @RequestMapping("api/public")
 public class PublicRestApiController {
-    private UserRepository userRepository;
+	private UserRepository userRepository;
 
-    public PublicRestApiController(UserRepository userRepository){
-        this.userRepository = userRepository;
-    }
+	public PublicRestApiController(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
 
-    @GetMapping("test1")
-    public String test1(){
-        return "API Test 1";
-    }
+	@GetMapping("test1")
+	public String test1() {
+		return "API Test 1";
+	}
 
-    @GetMapping("test2")
-    public String test2(){
-        return "API Test 2";
-    }
+	@GetMapping("test2")
+	public String test2() {
+		return "API Test 2";
+	}
 
-    @GetMapping("users")
-    public List<User> users(){
-        return this.userRepository.findAll();
-    }
+	@GetMapping("users")
+	public List<User> users() {
+		return this.userRepository.findAll();
+	}
 }
