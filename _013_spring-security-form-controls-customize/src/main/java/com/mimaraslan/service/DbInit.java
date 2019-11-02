0103bfe3d644d4675f5029501af0c5ebd7d00d4e
mimaraslan <mimaraslan@gmail.com>
2019-11-02 16:04:22 +0300

    Spring Security

--- _013_spring-security-form-controls-customize/src/main/java/com/mimaraslan/service/DbInit.java
+++ _013_spring-security-form-controls-customize/src/main/java/com/mimaraslan/service/DbInit.java
@@ -25,14 +25,14 @@ public class DbInit implements CommandLineRunner {
         // Delete all
         this.userRepository.deleteAll();
 
-        // Crete users
+        // Create users
         User admin = new User("admin",passwordEncoder.encode("admin"),"ADMIN","ACCESS_TEST1,ACCESS_TEST2");
         User katerina = new User("katerina",passwordEncoder.encode("katerina"),"USER","");
         User manager = new User("manager",passwordEncoder.encode("manager"),"MANAGER","ACCESS_TEST1");
 
         List<User> users = Arrays.asList(admin,katerina,manager);
 
-        // Save to db
+        // Save to DB
         this.userRepository.saveAll(users);
     }
 }
