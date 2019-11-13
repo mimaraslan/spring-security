94287ce2e7ada90a4d0d4ff0774f67d24a835040
mimaraslan <mimaraslan@gmail.com>
2019-11-13 15:56:45 +0300

    Spring Security

--- _011_spring-security-form-logout/src/main/java/com/mimaraslan/service/UserService.java
+++ _011_spring-security-form-logout/src/main/java/com/mimaraslan/service/UserService.java
@@ -5,16 +5,17 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.mimaraslan.model.User;
+import com.mimaraslan.repository.UserRepository;
 
 import java.util.Arrays;
 import java.util.List;
 
 @Service
-public class DbInit implements CommandLineRunner {
+public class UserService implements CommandLineRunner {
     private UserRepository userRepository;
     private PasswordEncoder passwordEncoder;
 
-    public DbInit(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
