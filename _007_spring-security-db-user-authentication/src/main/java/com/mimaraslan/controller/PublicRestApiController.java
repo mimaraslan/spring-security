e7e3b37c9c523490d1a5cb7ec3b82dab1f840824
mimaraslan <mimaraslan@gmail.com>
2019-11-22 15:33:27 +0300

    Spring Security

--- _007_spring-security-db-user-authentication/src/main/java/com/mimaraslan/controller/PublicRestApiController.java
+++ _007_spring-security-db-user-authentication/src/main/java/com/mimaraslan/controller/PublicRestApiController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.mimaraslan.db.UserRepository;
 import com.mimaraslan.model.User;
+import com.mimaraslan.repository.UserRepository;
 
 @RestController
 @RequestMapping("api/public")
