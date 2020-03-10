b9ee330d30bc6f17656f072016379402aeb2c45f
mimaraslan <mimaraslan@gmail.com>
2020-03-11 00:55:59 +0300

    Spring Boot Security
    Spring Boot Security

--- _002_spring-security-http-basic-authentication/src/main/java/com/mimaraslan/MainApplication.java
+++ _002_spring-security-http-basic-authentication/src/main/java/com/mimaraslan/MainApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-
 @SpringBootApplication (exclude = {SecurityAutoConfiguration.class} )
 public class MainApplication {
 
