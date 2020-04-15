2995265e25f3183c6aa5af028e6491b29f7453ce
mimaraslan <mimaraslan@gmail.com>
2020-04-15 11:54:54 +0300

    Spring Security Permission Based Authorization
    Spring Security Permission Based Authorization

---
+++ _006_spring-security-permission-based-authorization/src/main/java/com/mimaraslan/controller/PublicRestApiController.java
@@ -0,0 +1,20 @@
+package com.mimaraslan.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/public")
+public class PublicRestApiController {
+
+	@GetMapping("test1")
+	public String test1() {
+		return "API Test 1";
+	}
+
+	@GetMapping("test2")
+	public String test2() {
+		return "API Test 2";
+	}
+}
---
+++ _006_spring-security-permission-based-authorization/src/main/java/com/mimaraslan/security/SecurityConfiguration.java
@@ -0,0 +1,58 @@
+package com.mimaraslan.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+        	.inMemoryAuthentication()
+        	
+        	.withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN").authorities("ACCESS_TEST1", "ACCESS_TEST2") // Permission Authorization
+        	
+        	.and()
+        	.withUser("katerina").password(passwordEncoder().encode("katerina")).roles("USER") 
+        	
+	        .and()
+        	.withUser("manager").password(passwordEncoder().encode("manager")).roles("MANAGER").authorities("ACCESS_TEST2"); // Permission Authorization
+        
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+             .authorizeRequests()
+             .antMatchers("/index.html").permitAll()
+             .antMatchers("/profile/**").authenticated()
+             .antMatchers("/admin/**").hasRole("ADMIN")
+             .antMatchers("/management/**").hasAnyRole("ADMIN", "MANAGER")
+            
+             // http://localhost:8080/api/public/test1
+             // .antMatchers("/api/public/test1").authenticated()
+             // .antMatchers("/api/public/test2").authenticated()
+             
+             // .antMatchers("/api/public/**").authenticated()
+             
+             // .antMatchers("/api/public/**").hasRole("ADMIN")
+               
+             .antMatchers("/api/public/test1").hasAuthority("ACCESS_TEST1")
+             .antMatchers("/api/public/test2").hasAuthority("ACCESS_TEST2")
+             .and()
+             .httpBasic();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
