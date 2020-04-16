8e5ec500d0f97aa01b3a467e9868b485d2f37f6b
mimaraslan <mimaraslan@gmail.com>
2020-04-16 11:58:41 +0300

    Spring Security DB user details Service
    Spring Security DB user details Service

---
+++ _009_spring-security-db-user-details-service2/src/main/java/com/mimaraslan/security/UserPrincipalDetailsService.java
@@ -0,0 +1,28 @@
+package com.mimaraslan.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.mimaraslan.model.User;
+import com.mimaraslan.repository.UserRepository;
+
+@Service
+public class UserPrincipalDetailsService implements UserDetailsService {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+    public UserPrincipalDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.userRepository.findByUsername(username);
+        UserPrincipal userPrincipal = new UserPrincipal(user);
+        return userPrincipal;
+    }
+} 
