7c2123ff75467a9032b465638f9f1c187026bb0e
mimaraslan <mimaraslan@gmail.com>
2020-04-16 11:57:11 +0300

    Spring Security DB User Authentication
    Spring Security DB User Authentication

---
+++ _008_spring-security-db-user-authentication/src/main/java/com/mimaraslan/service/UserService.java
@@ -0,0 +1,38 @@
+package com.mimaraslan.service;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.mimaraslan.model.User;
+import com.mimaraslan.repository.UserRepository;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class UserService implements CommandLineRunner {
+	private UserRepository userRepository;
+	private PasswordEncoder passwordEncoder;
+
+	public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+		this.userRepository = userRepository;
+		this.passwordEncoder = passwordEncoder;
+	}
+
+	@Override
+	public void run(String... args) {
+		// Delete all
+		this.userRepository.deleteAll();
+
+		// Create users
+		User admin = new User("admin", passwordEncoder.encode("admin"), "ADMIN", "ACCESS_TEST1,ACCESS_TEST2");
+		User katerina = new User("katerina", passwordEncoder.encode("katerina"), "USER", "");
+		User manager = new User("manager", passwordEncoder.encode("manager"), "MANAGER", "ACCESS_TEST1");
+
+		List<User> users = Arrays.asList(admin, katerina, manager);
+
+		// Save to DB
+		this.userRepository.saveAll(users);
+	}
+}
