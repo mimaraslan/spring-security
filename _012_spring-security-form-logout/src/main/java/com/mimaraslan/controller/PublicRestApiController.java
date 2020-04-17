aabbf988f31d2d3a73158210f2caff242e7f4e5c
mimaraslan <mimaraslan@gmail.com>
2020-04-17 12:03:44 +0300

    Spring Security Form Logout
    Spring Security Form Logout

---
+++ _012_spring-security-form-logout/src/main/java/com/mimaraslan/controller/PublicRestApiController.java
@@ -0,0 +1,35 @@
+package com.mimaraslan.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mimaraslan.model.User;
+import com.mimaraslan.repository.UserRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/public")
+public class PublicRestApiController {
+	private UserRepository userRepository;
+
+	public PublicRestApiController(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	@GetMapping("users")
+	public List<User> users() {
+		return this.userRepository.findAll();
+	}
+
+	@GetMapping("test1")
+	public String test1() {
+		return "API Test 1";
+	}
+
+	@GetMapping("test2")
+	public String test2() {
+		return "API Test 2";
+	}
+}
---
+++ _012_spring-security-form-logout/src/main/java/com/mimaraslan/security/SecurityConfiguration.java
@@ -0,0 +1,84 @@
+package com.mimaraslan.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.mimaraslan.service.LoggingAccessDeniedHandler;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    
+	@Autowired
+	private LoggingAccessDeniedHandler accessDeniedHandler;
+	
+	private UserPrincipalDetailsService userPrincipalDetailsService;
+
+    public SecurityConfiguration(UserPrincipalDetailsService userPrincipalDetailsService) {
+        this.userPrincipalDetailsService = userPrincipalDetailsService;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers(
+                        "/",
+                        "/js/**",
+                        "/css/**",
+                        "/img/**",
+                        "/webjars/**").permitAll()
+                .antMatchers("/index.html").permitAll()
+                .antMatchers("/profile/**").authenticated()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/management/**").hasAnyRole("ADMIN", "MANAGER")
+                .antMatchers("/api/public/test1").hasAuthority("ACCESS_TEST1")
+                .antMatchers("/api/public/test2").hasAuthority("ACCESS_TEST2")
+                .antMatchers("/api/public/users").hasRole("ADMIN")
+                
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/signin")  
+                .defaultSuccessUrl("/profile/index")
+                .permitAll()    
+
+                .and()
+                .logout()
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+		        .logoutSuccessUrl("/login?logout")
+                
+                .and()
+                .exceptionHandling()
+                .accessDeniedHandler(accessDeniedHandler);
+    }
+
+    @Bean
+    DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(this.userPrincipalDetailsService);
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
