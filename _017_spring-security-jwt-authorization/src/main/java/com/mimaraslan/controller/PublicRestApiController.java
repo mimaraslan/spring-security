1c3be9f1905e5c45f82dd334ace9442da5062acd
mimaraslan <mimaraslan@gmail.com>
2020-04-19 12:10:15 +0300

    Spring Security JWT Authorization
    Spring Security JWT Authorization

---
+++ _017_spring-security-jwt-authorization/src/main/java/com/mimaraslan/controller/PublicRestApiController.java
@@ -0,0 +1,40 @@
+package com.mimaraslan.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mimaraslan.model.User;
+import com.mimaraslan.repository.UserRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/public")
+@CrossOrigin
+public class PublicRestApiController {
+	private UserRepository userRepository;
+
+	public PublicRestApiController(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	// Available to all authenticated users
+	@GetMapping("test")
+	public String test1() {
+		return "API Test";
+	}
+
+	// Available to managers
+	@GetMapping("management/reports")
+	public String reports() {
+		return "Some report data";
+	}
+
+	// Available to ROLE_ADMIN
+	@GetMapping("admin/users")
+	public List<User> users() {
+		return this.userRepository.findAll();
+	}
+}
---
+++ _017_spring-security-jwt-authorization/src/main/java/com/mimaraslan/security/SecurityConfiguration.java
@@ -0,0 +1,55 @@
+package com.mimaraslan.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.mimaraslan.repository.UserRepository;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    private UserPrincipalDetailsService userPrincipalDetailsService;
+    private UserRepository userRepository;
+
+    public SecurityConfiguration(UserPrincipalDetailsService userPrincipalDetailsService, UserRepository userRepository) {
+        this.userPrincipalDetailsService = userPrincipalDetailsService;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+        	.authorizeRequests()
+        	.antMatchers("*").permitAll();
+        // https://localhost:8443/api/public/test
+        // https://localhost:8443/api/public/management/reports
+        // https://localhost:8443/api/public/admin/users
+    }
+
+    @Bean
+    DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(this.userPrincipalDetailsService);
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
